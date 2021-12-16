97c10cc408a403c797c4e3650686c2f2d93873cf
GyuYeong <77821573+SimGyuYeong@users.noreply.github.com>
2021-12-16 20:26:41 +0900

    Json 버그수정

--- Assets/Script/Json.cs
+++ Assets/Script/Json.cs
@@ -39,7 +39,17 @@ public class Json : MonoBehaviour
 
     public void LoadFromJson()
     {
-        string saveData = File.ReadAllText(Path.Combine(Application.dataPath, "saveData.json"));
-        data = JsonConvert.DeserializeObject <Dictionary<int, saveData>> (saveData);
+        if (File.Exists(string.Concat(Application.dataPath, "saveData.json"){
+            string saveData = File.ReadAllText(Path.Combine(Application.dataPath, "saveData.json"));
+            data = JsonConvert.DeserializeObject<Dictionary<int, saveData>>(saveData);
+        }
+        else
+        {
+            data[1] = new saveData("Save Slot 1", 0, " ");
+            data[2] = new saveData("Save Slot 2", 0, " ");
+            data[3] = new saveData("Save Slot 3", 0, " ");
+            SaveToJson();
+        }
+        
     }
 }
