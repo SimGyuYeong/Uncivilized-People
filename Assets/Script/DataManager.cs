662cae2285f8cc99b4160ec21dd4fea7b1d68703
GyuYeong <77821573+SimGyuYeong@users.noreply.github.com>
2021-12-22 00:56:44 +0900

    세이브 UI 작업

--- Assets/Script/DataManager.cs
+++ Assets/Script/DataManager.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 using UnityEngine.UI;
 using System.IO;
 using Newtonsoft.Json;
+using System;
 
 class saveData
 {
@@ -19,25 +20,56 @@ class saveData
     }
 }
 
-public class Json : MonoBehaviour
+public class DataManager : MonoBehaviour
 {
     Dictionary<int, saveData> data = new Dictionary<int, saveData>();
 
     [SerializeField] private GameObject savemenuPanel;
     [SerializeField] List<GameObject> menu = new List<GameObject>();
 
+    private int SLCheck = 1;
+
     private void Start()
     {
         LoadFromJson();
-        for(int i = 0; i < 8; i++)
+        SaveMenuPanelUpdate();
+    }
+
+    private void SaveMenuPanelUpdate()
+    {
+        for (int i = 0; i < 8; i++)
         {
-            for(int j = 1; j < 3; j++)
+            for (int j = 1; j < 3; j++)
             {
                 GameObject obj = menu[i].transform.GetChild(j).gameObject;
                 Text text = obj.GetComponent<Text>();
                 if (j == 0) text.text = data[i].playerName;
                 else text.text = data[i].date;
             }
+        }
+    }
+
+    public void SaveMenuPanelOpen(int num)
+    {
+        savemenuPanel.SetActive(true);
+        SLCheck = num;
+    }
+
+    public void SaveOrLoad(int slot)
+    {
+        if (SLCheck == 1)
+        {
+            Debug.Log("Save Complete");
+            data[slot].playerName = "테스트";
+            data[slot].date = DateTime.Now.ToString("yyyy년 MM월 dd일\n HH:mm:ss");
+            SaveToJson();
+            SaveMenuPanelUpdate();
+            savemenuPanel.SetActive(false);
+        }
+        else if (SLCheck == 2)
+        {
+            Debug.Log(data[slot].playerName);
+            Debug.Log(data[slot].date);
         }
     }
 
